ffef7d9b861647698b1392ab0719a643a045ea5d
yotaro <yotaro.ok@gmail.com>
2020-09-26 21:28:26 +0900

    upd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,7 +90,6 @@
         </div>
     </body>
     <script type="text/javascript" src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('js/sampleData.js') }}"></script>
     <script type="text/javascript">
         $(document).ready(function(){
             $.ajax({
